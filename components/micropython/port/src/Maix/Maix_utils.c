b1535c25d1af7c158c1ebe575574aeb92e28e7e4
Robey Pointer <robeypointer@gmail.com>
2022-06-18 12:26:52 -0700

    add sys_heap_info for better heap stats

--- components/micropython/port/src/Maix/Maix_utils.c
+++ components/micropython/port/src/Maix/Maix_utils.c
@@ -1,3 +1,4 @@
+#include <malloc.h>
 #include "py/obj.h"
 #include "py/runtime.h"
 #include "py/mperrno.h"
@@ -28,6 +29,44 @@ STATIC mp_obj_t py_heap_free(void) {
     return MP_OBJ_NEW_SMALL_INT(get_free_heap_size2());
 }
 MP_DEFINE_CONST_FUN_OBJ_0(py_heap_free_obj, py_heap_free);
+
+STATIC mp_obj_t py_sys_heap_info(void) {
+    extern char _ram_start[];
+    extern char _heap_start[];
+    extern size_t _heap_cur;
+
+    // an absurdly large amount of ram is used by the OS itself: more than
+    // 3.5MB. some of this is micropython's heap and static allocations by
+    // the platform, but yikes!
+    uint32_t os_used = (uint32_t)((size_t)&_heap_start[0] - (size_t)&_ram_start[0]);
+
+    // get_free_heap_size2() is effectively the difference between _heap_end
+    // and _heap_cur (with _heap_end modified to account for space used by
+    // _iomem_line).
+    uint32_t heap_avail = (uint32_t)get_free_heap_size2();
+    // heap_used should be the same as arena_size, but include it as a check.
+    uint32_t heap_used = (uint32_t)(_heap_cur - (size_t)&_heap_start[0]);
+
+    // here's a fun one: gcc fills in `mallinfo` as if each field is a u32.
+    // but kendryte reads the struct as if each field is u64. we need to
+    // cast back.
+    struct mallinfo info = mallinfo();
+    uint32_t *real_info = (uint32_t *)&info;
+    uint32_t arena_size = real_info[0], arena_used = real_info[7], arena_free = real_info[8];
+
+    // os_used + heap_used + heap_avail = total system memory (6MB)
+    // arena_used + arena_free = arena_size = heap_used
+    mp_obj_t items[6] = {
+        mp_obj_new_int_from_uint(os_used),
+        mp_obj_new_int_from_uint(heap_used),
+        mp_obj_new_int_from_uint(heap_avail),
+        mp_obj_new_int_from_uint(arena_size),
+        mp_obj_new_int_from_uint(arena_used),
+        mp_obj_new_int_from_uint(arena_free),
+    };
+    return mp_obj_new_tuple(6, items);
+}
+MP_DEFINE_CONST_FUN_OBJ_0(py_sys_heap_info_obj, py_sys_heap_info);
 
 // STATIC mp_obj_t py_malloc(mp_obj_t arg) {
 //     void malloc_stats(void);
@@ -74,6 +113,7 @@ static const mp_map_elem_t locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_heap_free),    (mp_obj_t)(&py_heap_free_obj) },
     // { MP_ROM_QSTR(MP_QSTR_malloc),    (mp_obj_t)(&py_malloc_obj) },
     // { MP_ROM_QSTR(MP_QSTR_free),    (mp_obj_t)(&py_free_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sys_heap_info),   (mp_obj_t)(&py_sys_heap_info_obj) },
     { MP_ROM_QSTR(MP_QSTR_flash_read),    (mp_obj_t)(&py_flash_read_obj) },
     // { MP_ROM_QSTR(MP_QSTR_flash_write),    (mp_obj_t)(&py_flash_write_obj) },
 };
